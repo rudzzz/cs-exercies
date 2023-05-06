649e40630fee710a0d7be73868efef6a0a7837d0
rudzzz <rudsolza13@gmail.com>
2023-05-06 10:42:59 -0300

    second exercise

---
+++ Exercise 02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 1;
+
+            while(option == 1)
+            {
+                Console.WriteLine("Say me a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                option = 0;
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"\nThe number {number} is even!\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\nThe number {number} is odd!\n");
+                }
+
+                Console.WriteLine("\nWould you like to see another number?If so, please type 1");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
---
+++ Exercise 02/README.md
@@ -0,0 +1,15 @@
+﻿<h1>Exercise 02</h1> 
+
+## Description
+
+<p align="justify">
+Problem: Create a program that takes in a number from the user and displays whether it is odd or even.
+
+Overview: This C# program prompts the user to enter a number, checks whether the number is odd or even, and then asks the user whether they would like to enter another number. The program will continue running until the user decides to exit by typing any number other than 1.
+</p>
+
+## Languages used :books:
+
+- [C#]
+
+Copyright :copyright: 2023 - Exercise 02
