c0d5cbd5c2fade333a9250218ab98635499adbe5
rudzzz <rudsolza13@gmail.com>
2023-06-29 19:18:11 -0300

    User Login Project

---
+++ NoteApp/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NoteApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textTitle = new System.Windows.Forms.TextBox();
+			this.textMessage = new System.Windows.Forms.TextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.buttonNew = new System.Windows.Forms.Button();
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.buttonRead = new System.Windows.Forms.Button();
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(19, 76);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(84, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Title";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(19, 216);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(126, 29);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Message";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// textTitle
+			// 
+			this.textTitle.Location = new System.Drawing.Point(151, 73);
+			this.textTitle.Multiline = true;
+			this.textTitle.Name = "textTitle";
+			this.textTitle.Size = new System.Drawing.Size(194, 32);
+			this.textTitle.TabIndex = 2;
+			// 
+			// textMessage
+			// 
+			this.textMessage.Location = new System.Drawing.Point(151, 130);
+			this.textMessage.Multiline = true;
+			this.textMessage.Name = "textMessage";
+			this.textMessage.Size = new System.Drawing.Size(194, 214);
+			this.textMessage.TabIndex = 3;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.dataGridView1.Location = new System.Drawing.Point(379, 73);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.Size = new System.Drawing.Size(324, 271);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// buttonNew
+			// 
+			this.buttonNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonNew.Location = new System.Drawing.Point(151, 376);
+			this.buttonNew.Name = "buttonNew";
+			this.buttonNew.Size = new System.Drawing.Size(86, 31);
+			this.buttonNew.TabIndex = 5;
+			this.buttonNew.Text = "New";
+			this.buttonNew.UseVisualStyleBackColor = true;
+			this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonSave.Location = new System.Drawing.Point(299, 376);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(86, 31);
+			this.buttonSave.TabIndex = 6;
+			this.buttonSave.Text = "Save";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+			// 
+			// buttonRead
+			// 
+			this.buttonRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonRead.Location = new System.Drawing.Point(443, 376);
+			this.buttonRead.Name = "buttonRead";
+			this.buttonRead.Size = new System.Drawing.Size(86, 31);
+			this.buttonRead.TabIndex = 7;
+			this.buttonRead.Text = "Read";
+			this.buttonRead.UseVisualStyleBackColor = true;
+			this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonDelete.Location = new System.Drawing.Point(617, 376);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(86, 31);
+			this.buttonDelete.TabIndex = 8;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.UseVisualStyleBackColor = true;
+			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(124, 9);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(504, 38);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Note Taking Application";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(753, 459);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.buttonDelete);
+			this.Controls.Add(this.buttonRead);
+			this.Controls.Add(this.buttonSave);
+			this.Controls.Add(this.buttonNew);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.textMessage);
+			this.Controls.Add(this.textTitle);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "NoteApp";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textTitle;
+		private System.Windows.Forms.TextBox textMessage;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button buttonNew;
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.Button buttonRead;
+		private System.Windows.Forms.Button buttonDelete;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
